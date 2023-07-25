26315165af54d69774e0d48189445ab3b531a293
mjlover05 <93177731+mjlover05@users.noreply.github.com>
2023-07-26 02:07:56 +0530

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// https://github.com/projectsophon/hardhat-circom
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+
+// set env var to the root of the project
+process.env.BASE_PATH = __dirname;
+
+// tasks
+import "./tasks/newcircuit.ts"
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        "6b16f9c40a50b61e7cab952555082e1fa003926e30fbe50f84bbac0af0cb6a02",
+      ],
+    },
+  },
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+  polygonscan:{
+    Api_key:"B3CA5XKK1Q2T2JHH8T4MVJ923KCIBPNZ3H"
+  }
+};
+
+export default config;
